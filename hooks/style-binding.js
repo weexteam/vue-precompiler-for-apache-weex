189029e7d7deb13b4cbcfd56847fda32fde55d00
MrRaindrop <tekkahs@gmail.com>
2017-12-05 22:49:22 +0800

    parse styleBinding with esprima.

--- hooks/style-binding.js
+++ hooks/style-binding.js
@@ -1,3 +1,10 @@
+const esprima = require('esprima')
+const escodegen = require('escodegen')
+
+/**
+ * to wrap object leterals with _px2rem()
+ */
+
 const bindingStyleNamesForPx2Rem = [
   'width',
   'height',
@@ -24,7 +31,41 @@ const bindingStyleNamesForPx2Rem = [
   'fontSize'
 ]
 
-const ruleReg =/,?([^,:]+):([^,(]+(?:\([^)]+\)[^,}]*)?)/g
+// // for test
+// const exp = `a = { 'width': 'w', height: h, bgColor: _px2rem(bgColor, 75)}`
+// const ast = esprima.parse(exp).body[0].expression.right
+// console.log(JSON.stringify(ast, null, 2))
+// transformObject(ast)
+// let res = escodegen.generate(ast, {
+//   format: {
+//     indent: {
+//       style: ' '
+//     },
+//     newline: '',
+//   }
+// })
+// console.log('res:', res)
+
+function transformObject (obj) {
+  const properties = obj.properties
+  for (let i = 0, l = properties.length; i < l; i++) {
+    const prop = properties[i]
+    const keyNode = prop.key
+    const keyType = keyNode.type
+    const key = keyType === 'Literal' ? keyNode.value : keyNode.name
+    const valNode = prop.value
+    if (bindingStyleNamesForPx2Rem.indexOf(key) > -1) {
+      prop.value = {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: '_px2rem'
+        },
+        arguments: [valNode, { type: 'Literal', value: 75 }]
+      }
+    }
+  }
+}
 
 module.exports = function styleBindingHook (
   el,
@@ -37,24 +78,34 @@ module.exports = function styleBindingHook (
   if (!styleBinding) {
     return
   }
-  const content = styleBinding.trim()
-    .replace(/\s*[{}]\s*/g, '')
-  let match
-  let res = '{'
-  while ((match = ruleReg.exec(content))) {
-    try {
-      let k = match[1]
-      let v = match[2]
-      k = k && k.trim()
-      v = v && v.trim()
-      if (k && bindingStyleNamesForPx2Rem.indexOf(k) > -1) {
-        v = `_px2rem(${v},75)`
+  const statement = `a = ${styleBinding.trim()}`
+  const ast = esprima.parse(statement).body[0].expression.right
+  if (ast.type === 'Identifier') {
+    return
+  }
+
+  if (ast.type === 'ArrayExpression') {
+    const elements = ast.elements
+    for (let i = 0, l = elements.length; i < l; i++) {
+      const element = elements[i]
+      if (element.type === 'ObjectExpression') {
+        transformObject(element)
       }
-      res += `${k}:${v},`
-    } catch (err) {
-      console.error('[weex-vue-precompiler] style binding match error:', str)
     }
   }
-  res = res.substr(0, res.length - 1) + '}'
-  el.styleBinding = res
+  else if (ast.type === 'ObjectExpression') {
+    transformObject(ast)
+  }
+  else {
+    return
+  }
+
+  el.styleBinding = escodegen.generate(ast, {
+    format: {
+      indent: {
+        style: ' '
+      },
+      newline: '',
+    }
+  })
 }
