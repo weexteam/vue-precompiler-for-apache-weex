5a8bc08386253d585a9d904ef1277e2ca9e70f42
Zhenfei You <hello@imyzf.com>
2018-07-31 17:27:24 +0800

    fix: disable cache of parseAst
    fix problem '_px2rem(_px2rem(_px2rem(…)))'

--- src/util/ast.js
+++ src/util/ast.js
@@ -18,23 +18,16 @@ exports.genPropNode = function (k, v) {
   }
 }
 
-exports.parseAst = (function () {
-  const _cached = {}
-  return function (val) {
-    let statement = 'a = '
-    if (typeof val === 'object') {
-      statement += `${JSON.stringify(val)}`
-    }
-    else {
-      statement += val
-    }
-    const cached = _cached[statement]
-    if (cached) {
-      return cached
-    }
-    const ast = esprima.parse(statement)
-      .body[0].expression.right
-    _cached[statement] = ast
-    return ast
+exports.parseAst = function (val) {
+  let statement = 'a = '
+  if (typeof val === 'object') {
+    statement += `${JSON.stringify(val)}`
   }
-})()
+  else {
+    statement += val
+  }
+  const ast = esprima.parse(statement)
+    .body[0].expression.right
+  return ast
+}
+
